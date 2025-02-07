5ca5e2634afee56beb21ab62067c105f7a0c03a2
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-07 20:21:42 +0000

    Reset to 6c0ae3e4505bcc28ef745f8caaea5411fa01a4fd and made changes for LLM Round 1

--- beaverhabits/storage/dict.py
+++ beaverhabits/storage/dict.py
@@ -1,6 +1,6 @@
 import datetime
 from dataclasses import dataclass, field
-from typing import List, Optional
+from typing import Optional
 
 from beaverhabits.storage.storage import CheckedRecord, Habit, HabitList
 from beaverhabits.utils import generate_short_hash
@@ -8,73 +8,55 @@ from beaverhabits.utils import generate_short_hash
 DAY_MASK = "%Y-%m-%d"
 MONTH_MASK = "%Y/%m"
 
-
 @dataclass(init=False)
-class DictStorage:
-    data: dict = field(default_factory=dict, metadata={"exclude": True})
-
+default_factory=dict)
+default_factory=list)
 
 @dataclass
 class DictRecord(CheckedRecord, DictStorage):
-    """
-    # Read (d1~d3)
-    persistent    ->     memory      ->     view
-    d0: [x]              d0: [x]
-                                            d1: [ ]
-    d2: [x]              d2: [x]            d2: [x]
-                                            d3: [ ]
-
-    # Update:
-    view(update)  ->     memory      ->     persistent
-    d1: [ ]
-    d2: [ ]              d2: [ ]            d2: [x]
-    d3: [x]              d3: [x]            d3: [ ]
-    """
-
     @property
-    def day(self) -> datetime.date:
+def day(self) -> datetime.date:
         date = datetime.datetime.strptime(self.data["day"], DAY_MASK)
         return date.date()
 
     @property
-    def done(self) -> bool:
+def done(self) -> bool:
         return self.data["done"]
 
     @done.setter
-    def done(self, value: bool) -> None:
+def done(self, value: bool) -> None:
         self.data["done"] = value
-
 
 @dataclass
 class DictHabit(Habit[DictRecord], DictStorage):
     @property
-    def id(self) -> str:
+def id(self) -> str:
         if "id" not in self.data:
             self.data["id"] = generate_short_hash(self.name)
         return self.data["id"]
 
     @id.setter
-    def id(self, value: str) -> None:
+def id(self, value: str) -> None:
         self.data["id"] = value
 
     @property
-    def name(self) -> str:
+def name(self) -> str:
         return self.data["name"]
 
     @name.setter
-    def name(self, value: str) -> None:
+def name(self, value: str) -> None:
         self.data["name"] = value
 
     @property
-    def star(self) -> bool:
+def star(self) -> bool:
         return self.data.get("star", False)
 
     @star.setter
-    def star(self, value: int) -> None:
+def star(self, value: int) -> None:
         self.data["star"] = value
 
     @property
-    def records(self) -> list[DictRecord]:
+def records(self) -> list[DictRecord]:
         return [DictRecord(d) for d in self.data["records"]]
 
     async def tick(self, day: datetime.date, done: bool) -> None:
@@ -103,38 +85,14 @@ class DictHabit(Habit[DictRecord], DictStorage):
     def __hash__(self) -> int:
         return hash(self.id)
 
-    def __str__(self) -> str:
-        return f"{self.name}<{self.id}>"
-
-    __repr__ = __str__
-
-
 @dataclass
 class DictHabitList(HabitList[DictHabit], DictStorage):
 
     @property
-    def habits(self) -> list[DictHabit]:
+def habits(self) -> list[DictHabit]:
         habits = [DictHabit(d) for d in self.data["habits"]]
-        if self.order:
-            habits.sort(
-                key=lambda x: (
-                    self.order.index(str(x.id))
-                    if str(x.id) in self.order
-                    else float("inf")
-                )
-            )
-        else:
-            habits.sort(key=lambda x: x.star, reverse=True)
-
+        habits.sort(key=lambda x: x.star, reverse=True)
         return habits
-
-    @property
-    def order(self) -> List[str]:
-        return self.data.get("order", [])
-
-    @order.setter
-    def order(self, value: List[str]) -> None:
-        self.data["order"] = value
 
     async def get_habit_by(self, habit_id: str) -> Optional[DictHabit]:
         for habit in self.habits:
