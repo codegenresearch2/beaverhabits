38bd6b065893f5b6efcb7a9385ce19f0e6aad994
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 12:26:14 +0000

    Reset to 834b9257c7775e162342f4db1f282fbc3dbaf497 and made changes for LLM Round 1

--- beaverhabits/storage/__init__.py
+++ beaverhabits/storage/__init__.py
@@ -1,6 +1,3 @@
-# The code snippet provided does not contain any string literals, so I am unable to identify the source of the SyntaxError mentioned in the test case feedback.
-# However, I will ensure that the code is properly formatted and follows best practices to avoid any potential syntax errors.
-
 from beaverhabits.configs import StorageType, settings
 from beaverhabits.storage.session_file import SessionDictStorage, SessionStorage
 from beaverhabits.storage.storage import UserStorage
@@ -18,7 +15,6 @@ def get_sessions_storage() -> SessionStorage:
 def get_user_dict_storage() -> UserStorage:
     if settings.HABITS_STORAGE == StorageType.USER_DISK:
         return user_disk_storage
-    elif settings.HABITS_STORAGE == StorageType.USER_DATABASE:
+    if settings.HABITS_STORAGE == StorageType.USER_DATABASE:
         return user_database_storage
-    else:
-        raise NotImplementedError("Storage type not implemented")
+    raise NotImplementedError("Storage type not implemented")
