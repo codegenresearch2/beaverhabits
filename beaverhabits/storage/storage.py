c6820f539c2e7dec31f618c21c49a08b42a84f8d
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 10:30:48 +0000

    Reset to 834b9257c7775e162342f4db1f282fbc3dbaf497 and made changes for LLM Round 1

--- beaverhabits/storage/storage.py
+++ beaverhabits/storage/storage.py
@@ -62,16 +62,6 @@ class HabitList[H: Habit](Protocol):
 
     async def get_habit_by(self, habit_id: str) -> Optional[H]: ...
 
-    async def merge(self, other: "HabitList[H]") -> "HabitList[H]":
-        self_habits = set(self.habits)
-        other_habits = set(other.habits)
-        merged_habits = self_habits.symmetric_difference(other_habits)
-
-        for habit in self_habits.intersection(other_habits):
-            merged_habits.add(await habit.merge(other_habit))
-
-        return HabitList([h for h in merged_habits])
-
 
 class SessionStorage[L: HabitList](Protocol):
     def get_user_habit_list(self) -> Optional[L]: ...
@@ -83,3 +73,5 @@ class UserStorage[L: HabitList](Protocol):
     async def get_user_habit_list(self, user: User) -> Optional[L]: ...
 
     async def save_user_habit_list(self, user: User, habit_list: L) -> None: ...
+
+    async def merge_user_habit_list(self, user: User, habit_list: L) -> None: ...
