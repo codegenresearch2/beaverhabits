1ca96953619adedb246444cac58e00229eda9f22
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 09:29:09 +0000

    Reset to 834b9257c7775e162342f4db1f282fbc3dbaf497 and made changes for LLM Round 1

--- beaverhabits/storage/storage.py
+++ beaverhabits/storage/storage.py
@@ -1,8 +1,5 @@
-import datetime
-from typing import List, Optional, Protocol
-
+from typing import Generic, List, Protocol, TypeVar
 from beaverhabits.app.db import User
-
 
 class CheckedRecord(Protocol):
     @property
@@ -19,8 +16,9 @@ class CheckedRecord(Protocol):
 
     __repr__ = __str__
 
+T = TypeVar('T')
 
-class Habit(Protocol):
+class Habit(Generic[T], Protocol):
     @property
     def id(self) -> str: ...
 
@@ -37,7 +35,7 @@ class Habit(Protocol):
     def star(self, value: bool) -> None: ...
 
     @property
-    def records(self) -> List[CheckedRecord]: ...
+    def records(self) -> List[T]: ...
 
     @property
     def ticked_days(self) -> list[datetime.date]:
@@ -50,52 +48,47 @@ class Habit(Protocol):
 
     __repr__ = __str__
 
-
-class HabitList(Protocol):
+class HabitList(Generic[T], Protocol):
     @property
-    def habits(self) -> List[Habit]: ...
+    def habits(self) -> List[T]: ...
 
     async def add(self, name: str) -> None: ...
 
-    async def remove(self, item: Habit) -> None: ...
+    async def remove(self, item: T) -> None: ...
 
-    async def get_habit_by(self, habit_id: str) -> Optional[Habit]: ...
+    async def get_habit_by(self, habit_id: str) -> Optional[T]: ...
 
-    async def merge(self, other: "HabitList") -> "HabitList": ...
-
+    async def merge(self, other: "HabitList[T]") -> "HabitList[T]": ...
 
 class SessionStorage(Protocol):
-    def get_user_habit_list(self) -> Optional[HabitList]: ...
+    def get_user_habit_list(self) -> Optional[HabitList[Habit[CheckedRecord]]]: ...
 
-    def save_user_habit_list(self, habit_list: HabitList) -> None: ...
-
+    def save_user_habit_list(self, habit_list: HabitList[Habit[CheckedRecord]]) -> None: ...
 
 class UserStorage(Protocol):
-    async def get_user_habit_list(self, user: User) -> Optional[HabitList]: ...
+    async def get_user_habit_list(self, user: User) -> Optional[HabitList[Habit[CheckedRecord]]]: ...
 
-    async def save_user_habit_list(self, user: User, habit_list: HabitList) -> None: ...
-
+    async def save_user_habit_list(self, user: User, habit_list: HabitList[Habit[CheckedRecord]]) -> None: ...
 
 class HabitManager:
-    def __init__(self, habit_list: HabitList):
+    def __init__(self, habit_list: HabitList[Habit[CheckedRecord]]):
         self.habit_list = habit_list
 
     async def add_habit(self, name: str) -> None:
         await self.habit_list.add(name)
 
-    async def remove_habit(self, habit: Habit) -> None:
+    async def remove_habit(self, habit: Habit[CheckedRecord]) -> None:
         await self.habit_list.remove(habit)
 
-    async def get_habit_by_id(self, habit_id: str) -> Optional[Habit]:
+    async def get_habit_by_id(self, habit_id: str) -> Optional[Habit[CheckedRecord]]:
         return await self.habit_list.get_habit_by(habit_id)
 
-    async def merge_habits(self, other_habit_list: HabitList) -> None:
+    async def merge_habits(self, other_habit_list: HabitList[Habit[CheckedRecord]]) -> None:
         await self.habit_list.merge(other_habit_list)
 
-    async def tick_habit(self, habit: Habit, day: datetime.date, done: bool) -> None:
+    async def tick_habit(self, habit: Habit[CheckedRecord], day: datetime.date, done: bool) -> None:
         await habit.tick(day, done)
 
-
-async def merge_habit_lists(habit_list1: HabitList, habit_list2: HabitList) -> HabitList:
+async def merge_habit_lists(habit_list1: HabitList[Habit[CheckedRecord]], habit_list2: HabitList[Habit[CheckedRecord]]) -> HabitList[Habit[CheckedRecord]]:
     merged_habits = await habit_list1.merge(habit_list2)
     return merged_habits
