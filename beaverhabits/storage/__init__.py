90139f1ad5db3b29e33b1c64abb61b78801685cf
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 12:32:41 +0000

    Reset to 834b9257c7775e162342f4db1f282fbc3dbaf497 and made changes for LLM Round 1

--- beaverhabits/storage/__init__.py
+++ beaverhabits/storage/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from typing import Optional
+
 from beaverhabits.configs import StorageType, settings
 from beaverhabits.storage.session_file import SessionDictStorage, SessionStorage
 from beaverhabits.storage.storage import UserStorage
@@ -9,16 +12,16 @@ user_disk_storage = UserDiskStorage()
 user_database_storage = UserDatabaseStorage()
 sqlite_storage = None
 
+logging.basicConfig(level=logging.INFO)
 
 def get_sessions_storage() -> SessionStorage:
     return session_storage
 
-
-def get_user_dict_storage() -> UserStorage:
+def get_user_storage() -> UserStorage:
     if settings.HABITS_STORAGE == StorageType.USER_DISK:
         return user_disk_storage
-
-    if settings.HABITS_STORAGE == StorageType.USER_DATABASE:
+    elif settings.HABITS_STORAGE == StorageType.USER_DATABASE:
         return user_database_storage
-
-    raise NotImplementedError("Storage type not implemented")
+    else:
+        logging.error("Storage type not implemented")
+        raise NotImplementedError("Storage type not implemented")
